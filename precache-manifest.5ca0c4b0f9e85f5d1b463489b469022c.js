22dda4a34dc2ec054d146a1b9075a3e85eba126d
patrykraj <patrykraj@interia.pl>
2020-03-07 19:25:33 +0100

    Updates

---
+++ precache-manifest.5ca0c4b0f9e85f5d1b463489b469022c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa8e8b18b487abe4e8273d691ca05893",
+    "url": "/CalculatorReactJS/index.html"
+  },
+  {
+    "revision": "8fd989de7d98885802c0",
+    "url": "/CalculatorReactJS/static/css/main.eb497bee.chunk.css"
+  },
+  {
+    "revision": "6f9ebea347346d44a1e2",
+    "url": "/CalculatorReactJS/static/js/2.50e7cce1.chunk.js"
+  },
+  {
+    "revision": "fab1da7d8a32be3fe90e332131eacf28",
+    "url": "/CalculatorReactJS/static/js/2.50e7cce1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fd989de7d98885802c0",
+    "url": "/CalculatorReactJS/static/js/main.24f3e3fa.chunk.js"
+  },
+  {
+    "revision": "a1ab28b6021c3a29b942",
+    "url": "/CalculatorReactJS/static/js/runtime-main.51bddf82.js"
+  }
+]);
